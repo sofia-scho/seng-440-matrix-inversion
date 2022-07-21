f6c5065f2c7ae5a92cb7f8b368052969c150d913
cartercooper <cartercooper@ugls20.ece.uvic.ca>
2022-07-20 21:56:53 -0700

    print matrix function

--- main.c
+++ main.c
@@ -1,7 +1,39 @@
-#include <stdio.h>
+#include "main.h"
 
 int main(void)
 {
-    printf("hello world");
+    // int result_matrix[MATRIX_SIZE][MATRIX_SIZE];
+
+    // invert_matrix((int**)test_matrix_1, (int**)result_matrix, MATRIX_SIZE);
+    // print_matrix((int**)result_matrix, MATRIX_SIZE);
+
+    // invert_matrix((int**)test_matrix_2, (int**)result_matrix, MATRIX_SIZE);
+    // print_matrix((int**)result_matrix, MATRIX_SIZE);
+
+    // invert_matrix((int**)test_matrix_3, (int**)result_matrix, MATRIX_SIZE);
+    // print_matrix((int**)result_matrix, MATRIX_SIZE);
+
+    print_matrix(test_matrix_1, MATRIX_SIZE);
+    
     return 0;
 }
+
+void invert_matrix(int (*matrix)[MATRIX_SIZE], int (*inverted_matrix)[MATRIX_SIZE] , int size)
+{
+
+}
+
+void print_matrix(int (*matrix)[MATRIX_SIZE], int matrix_size)
+{
+    int i, j;
+
+    for(i = 0; i < matrix_size; i++)
+    {
+        for(j = 0; j < matrix_size; j++)
+        {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
